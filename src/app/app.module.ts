325b87e18b440950c1be4776dac22196a6a0559d
Jean Pirul <jean.pirul@trebol-it.cl>
2021-11-25 18:44:21 -0300

    seccion 5 completa :D

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContadorModule } from './contador/contador.module';
+import { DbzModule } from './dbz/dbz.module';
 import { HeroesModule } from './heroes/heroes.modulo';
 
 //tarea crear modulo llamado contador.module
@@ -14,7 +15,8 @@ import { HeroesModule } from './heroes/heroes.modulo';
   imports: [
     BrowserModule,
     HeroesModule,
-    ContadorModule
+    ContadorModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
