c16b5e7e3da05654e38fadad99b5dc36a7b02609
Jean Pirul <jean.pirul@trebol-it.cl>
2021-11-25 11:54:53 -0300

    fin primer cursito angular seccion 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component';
-import { HeroeComponent } from './heroes/heroe/heroe.component';
-import { ListadoComponent } from './heroes/listado/listado.component';
+import { ContadorModule } from './contador/contador.module';
+import { HeroesModule } from './heroes/heroes.modulo';
 
+//tarea crear modulo llamado contador.module
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponent,
-    HeroeComponent,
-    ListadoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
